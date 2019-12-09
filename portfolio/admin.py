f617c2fac824e88d21f9a0486edd92fa9e3a25fe
PranshuVinayak <pranshuvinayak@gmail.com>
2019-12-09 20:54:28 +0530

    9Dec2019

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,17 +1,31 @@
 from django.contrib import admin
 from .models import Project, Category, Skill, Image
 from django.template.defaultfilters import truncatechars
+from django.utils.safestring import mark_safe
+import os  
+from django import forms
+from .models import ProjectForm
 # Register your models here.
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-admin.site.register(Category)
-admin.site.register(Skill)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'thumbnail', 'preview')
+    readonly_fields = ('preview', )
+admin.site.register(Category, CategoryAdmin)
+
+class SkillAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'thumbnail', 'preview')
+    readonly_fields = ('preview', )
+admin.site.register(Skill, SkillAdmin)
 
 class ImageAdmin(admin.ModelAdmin):                     # This would not show the `Images` Panel in the Admin List, but it can
     get_model_perms = lambda self, request: {}          # be Edited from the Projects Objects.
-
 admin.site.register(Image, ImageAdmin)
 
+class ImageInline(admin.StackedInline):
+    model = Image
+    extra = 0
 
 class ProjectAdmin(admin.ModelAdmin):
 
@@ -25,12 +39,42 @@ class ProjectAdmin(admin.ModelAdmin):
     skill.short_description = "skills"
 
     def desc(self, obj):
-        return truncatechars(obj.description, 10)
+        return truncatechars(obj.description, 60)
     desc.short_description = "description"
 
-    list_display = ['title', 'desc', 'skill', 'category']
+    def image_thumbnail(self, obj):
+        # print(os.path.join('/upload', os.path.basename(str(obj.thumbnail))))
+        return mark_safe('<img src="{}" width="50" height="30" />'.format(os.path.join('/Images/upload', os.path.basename(str(obj.thumbnail)))))
+    image_thumbnail.short_description = "Thumbnail"
+
+    form = ProjectForm
+    # inlines = [ImageInline]
+
+    def save_model(self, request, obj, form, change):
+        image_ids = request.POST.getlist("files[]")
+        print(image_ids)
+        obj.images.filter(project_id=obj.id).exclude(id__in=image_ids).delete()
+        super(ProjectAdmin,self).save_model(request, obj, form, change)
+
+        for afile in request.FILES.getlist('photos_multiple'):
+            # print('-----------------------------')
+            print(afile)
+            # print('-----------------------------')
+            obj.images.create(file=afile)
+
+    # def image_preview(self, obj):
+    #     images = ""
+    #     print(obj.images.all())
+    #     for image in obj.images.all():
+    #         print(image)
+    #         images += '<img src="{}" width="50" height="30" />'.format(os.path.join('/Images/upload', os.path.basename(str(image))))
+    #     return images
+
+    list_display = ['title', 'image_thumbnail', 'desc', 'skill', 'category']
     search_fields = ['title', 'description', 'skills__title']
 
-    
+    fields = ('title', 'description', 'link', 'thumbnail', 'preview', 'created_at', 'published_at', 'skills', 'categories')
+    # readonly_fields = ('preview', 'image_preview')
+    readonly_fields = ('preview',)
 admin.site.register(Project, ProjectAdmin)
 
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.utils import timezone
+import os
+from django.utils.safestring import mark_safe
+from django import forms
 
 
 class Skill(models.Model):
-    title = models.CharField(max_length=200, help_text="Change")
+    # title = models.CharField(max_length=200, help_text="Change")
+    title = models.CharField(max_length=200)
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='upload', null=True)
 
     def __str__(self):
         return self.title
+
+    def preview(self):
+        return mark_safe('<img src="{}" width="150" height="150" />'.format(os.path.join('/Images/upload', os.path.basename(str(self.thumbnail)))))
 
 class Category(models.Model):
     title = models.CharField(max_length=200)
@@ -22,12 +29,9 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
-class Image(models.Model):
-    file = models.ImageField(upload_to='upload')
-    position = models.PositiveSmallIntegerField(default=0)
-    
-    # def __str__(self):
-    #     return self.title
+    def preview(self):
+        return mark_safe('<img src="{}" width="150" height="150" />'.format(os.path.join('/Images/upload', os.path.basename(str(self.thumbnail)))))
+
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -38,7 +42,29 @@ class Project(models.Model):
     published_at = models.DateTimeField(blank=True, null=True)
     skills = models.ManyToManyField(Skill)
     categories = models.ManyToManyField(Category)
-    images = models.ManyToManyField(Image)
+    # images = models.ManyToManyField(Image)
 
     def __str__(self):
         return self.title
+
+    def preview(self):
+        return mark_safe('<img src="{}" width="150" height="150" />'.format(os.path.join('/Images/upload', os.path.basename(str(self.thumbnail)))))
+
+    # def image_preview(self):
+    #     print(self.images.all())
+
+class Image(models.Model):
+    file = models.ImageField(upload_to='upload')
+    position = models.PositiveSmallIntegerField(default=0)
+    project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['position']
+    
+    def __str__(self):
+        return str(self.file)
+        # print(str(self.file))
+        # return mark_safe('<img src="{}" width="150" height="150" />'.format(os.path.join('/Images/upload', os.path.basename(str(self.file)))))
+
+class ProjectForm(forms.ModelForm):
+    pass
